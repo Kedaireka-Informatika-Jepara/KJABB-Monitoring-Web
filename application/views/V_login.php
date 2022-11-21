69145e4d8939153935525b96399f2289229f7d9f
farrel216 <farrel.arp@gmail.com>
2022-11-21 21:13:16 +0700

    style login page

--- application/views/V_login.php
+++ application/views/V_login.php
@@ -11,11 +11,31 @@
 
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="icon" href="<?= base_url() ?>assets/dist/img/icon.png">
-  <link rel="stylesheet" href="<?= base_url() ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/animate.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/remixicon.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/boxicons.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/iconsax.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/metismenu.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/simplebar.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/calendar.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/sweetalert2.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/jbox.all.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/editor.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/loaders.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/header.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/sidebar-menu.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/footer.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/responsive.css">
+  <!-- <link rel="stylesheet" href="<?= base_url() ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets/bower_components/Ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/iCheck/square/blue.css"> -->
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> -->
 
   <!-- Google Font -->
@@ -44,26 +64,31 @@
       <img src="<?php echo base_url('assets/dist/img/lody.png'); ?>" width="50px" height="35px" style="margin-right: 15px;">
     </ul>
   </nav>
-  <div class="login-box" style="margin-top: 120px;">
-    <div class="login-box-body">
-      <p class="login-box-msg login">Login <br> Aplikasi Monitoring</p><br>
+
+  <div class="account-content">
+    <div class="account-header">
+      <a href="index.html">
+        <img src="assets/dist/img/kedaireka2.png" alt="main-logo">
+      </a>
       <?= $this->session->flashdata('info'); ?>
-
-      <form method="post" action="<?= base_url() ?>index.php/login/proses_login">
-        <div class="form-group has-feedback">
-          <input type="text" name="nama" class="form-control" placeholder="Username" required>
-          <span class="glyphicon glyphicon-user form-control-feedback"></span>
-        </div>
-
-        <div class="form-group has-feedback">
-          <input type="password" name="password" class="form-control" placeholder="Password" required>
-          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="d-grid gap-2 col-6 mx-auto">
-          <center><button type="submit" style="width: 100px;" class="btn btn-primary"> Login </button></center>
-        </div>
-      </form>
+      <h3>Login Aplikasi Monitoring</h3>
     </div>
+
+    <form class="account-wrap" method="post" action="<?= base_url() ?>index.php/login/proses_login">
+      <div class="form-group mb-24 icon">
+        <input type="text" name="nama" class="form-control" placeholder="Username" required>
+        <img src="<?= base_url() ?>assets/images/icon/sms.svg" alt="sms">
+      </div>
+      <div class="form-group mb-24 icon">
+        <input type="password" name="password" class="form-control" placeholder="Password">
+        <img src="<?= base_url() ?>assets/images/icon/key.svg" alt="key">
+      </div>
+      <div class="form-group mb-24">
+        <button type="submit" class="default-btn">Log In</button>
+      </div>
+    </form>
+
+
   </div>
 
   <script src="<?= base_url() ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
